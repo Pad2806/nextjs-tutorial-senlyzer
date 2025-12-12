cb21ff11333c56d962df266b25c63afc4912575a
Pad2806 <tuiladuyne28@gmail.com>
2025-12-13 03:30:52 +0700

    update example next-app

--- Day_4/nextjs-app/app/dashboard/users/[id]/edit/page.tsx
+++ Day_4/nextjs-app/app/dashboard/users/[id]/edit/page.tsx
@@ -1,11 +1,15 @@
 import Form from "@/app/ui/users/edit-form";
 import Breadcrumbs from "@/app/ui/users/breadcrumbs";
 import { fetchUserById } from "@/app/lib/data";
+import { notFound } from "next/navigation";
+
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
   const id = params.id;
   const [user] = await Promise.all([fetchUserById(id)]);
-
+  if (!user) {
+    notFound();
+  }
   return (
     <main>
       <Breadcrumbs
