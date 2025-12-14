fd50ced29cfcf3ebcb6708b3fbbc745f3cdfde7d
Pad2806 <tuiladuyne28@gmail.com>
2025-12-14 22:58:44 +0700

    update day 6

--- Day_6/nextjs-app/app/lib/actions.ts
+++ Day_6/nextjs-app/app/lib/actions.ts
@@ -4,6 +4,9 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import postgres from "postgres";
 import bcrypt from "bcryptjs";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
@@ -71,4 +74,23 @@ export async function updateUser(id: string, formData: FormData) {
 export async function deleteUser(id: string) {
   await sql`DELETE FROM users WHERE id = ${id}`;
   revalidatePath("/dashboard/users");
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
 }
