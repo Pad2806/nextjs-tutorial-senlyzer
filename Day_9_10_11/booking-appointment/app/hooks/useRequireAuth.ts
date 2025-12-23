f143704d4ac32e4aef7296e3b35b02b9c68e8209
Pad2806 <tuiladuyne28@gmail.com>
2025-12-23 18:11:13 +0700

    update admin landingbase day 13

--- Day_9_10_11/booking-appointment/app/hooks/useRequireAuth.ts
+++ Day_9_10_11/booking-appointment/app/hooks/useRequireAuth.ts
@@ -1,24 +1,3 @@
-// "use client";
-
-// import { useEffect } from "react";
-// import { useRouter } from "next/navigation";
-// import { useSession } from "next-auth/react";
-
-// export function useRequireAuth(redirectTo: string) {
-//   const { status } = useSession();
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     if (status === "unauthenticated") {
-//       router.push(`/login?next=${redirectTo}`);
-//     }
-//   }, [status, router, redirectTo]);
-
-//   return {
-//     isLoading: status === "loading",
-//     isAuthenticated: status === "authenticated",
-//   };
-// }
 "use client";
 
 import { useEffect } from "react";
